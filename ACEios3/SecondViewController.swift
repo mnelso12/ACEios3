152c99ba116abe95d2d8738a84270e27fdc660b4
Madelyn Nelson <mnelso12@nd.edu>
2017-01-20 16:48:12 -0500

    Initial Commit

---
+++ ACEios3/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ACEios3
+//
+//  Created by Madelyn Nelson on 1/20/17.
+//  Copyright © 2017 Madelyn Nelson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
