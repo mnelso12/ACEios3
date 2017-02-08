97db77fcc4ffa78570b7cbd7150fbf523059ce8e
Madelyn Nelson <mnelso12@nd.edu>
2017-02-07 19:46:19 -0500

    application checklist progress

--- ACEios3/ApplicationChecklistViewController.swift
+++ ACEios3/ApplicationChecklistViewController.swift
@@ -12,9 +12,24 @@ class ApplicationChecklistViewController: UIViewController, UITableViewDelegate,
 
     @IBOutlet weak var tableView: UITableView!
     
-    var contentArr = ["Teaching Fellows Application Checklist", "1. Online Application", "2. Transcript", "3. Four Reference Letters", "4. GRE"]
+    var countriesinEurope = ["France","Spain","Germany"]
+    var countriesinAsia = ["Japan","China","India"]
+    var countriesInSouthAmerica = ["Argentia","Brasil","Chile"]
     
-     let cellReuseIdentifier = "applyChecklistCell"
+    var titles = ["1. Application Type and Term", "2. Test Scores", "3. Academic History", "4. Additional Information", "5. Reference Letters", "6. Downloadable Forms", "7. Uploads", "8. Signature", "9. Review"]
+    var desc = ["Under \"Applicant type,\" select \"Degree.\" ACE Teaching Fellows is a full-time, degreeseeking program. Please select \"ACE Teaching Fellows - M.Ed.\" The admission term for this cohort is: Summer 2017. Because ACE Teaching Fellows is a cohort-based program, you do not need to select a faculty advisor. If you’ve previously been enrolled at Notre Dame, complete the appropriate section. If you are applying to any other programs at Notre Dame, please indicate that in the appropriate section.", "Complete the personal information page. Please enter your Skype username, as this may be used in interviews later on.",
+        "All applicants are required to submit GRE scores taken within the last 5 years. Notre Dame's insitution code is \"1841\"",
+        "For the sake of the application, please upload your unofficial transcript in this section. Your official transcripts will be required upon acceptance into ACE Teaching Fellows.",
+        "Complete the \"Rank Performance\" section for any other programs in which you are applying. Under \"Additional Questions,\" when asked if you will be paying your application fee with a waiver, click \"Yes.\" When prompted, enter the code \"ACE39.\"",
+        "You are required to have four reference letters: 2 from professors, 1 from a peer, and 1 from a residence hall director, campus minister, or service project leader who can attest to your success in living and working in a community atmosphere.",
+        "Download the ACE Teaching Fellows Supplement/Program Upload (\"ACE Teaching Fellows - M.Ed.\") and complete the form.",
+        "Please submit your \"Program Upload\" (\"ACE Teaching Fellows - M.Ed.\") and your Resume/CV in their related sections. Your photo can be submitted in the \"Additional Documents\" section. The Statement of Intent and a Writing Sample are NOT required for our program.",
+        "Input your electronic signature.",
+        "Review and confirm that you have completed all parts of the application and submit when complete."]
+    
+    
+    let cellReuseIdentifier = "applyChecklistCell"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,23 +37,54 @@ class ApplicationChecklistViewController: UIViewController, UITableViewDelegate,
         tableView.delegate = self
         tableView.dataSource = self
         
+        // so table cell height adjusts to fit content
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 200
+        
     }
 
-    // table view 
     
-    // number of rows in table view
+    
+    
+    
+    
+    // table view
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 9
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contentArr.count
+        return 1
+    }
+   
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
     }
     
     // create a cell for each table view row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
         
         // create a new cell if needed or reuse an old one
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! ApplicationTableViewCell
         
-        // set the text from the data model
-        cell.textLabel?.text = self.contentArr[indexPath.row]
+        /*
+        switch (indexPath.section) {
+        case 0:
+            cell.textLabel?.text = desc[index]
+        case 1:
+            cell.textLabel?.text = countriesinAsia[indexPath.row]
+        case 2:
+            cell.textLabel?.text = countriesInSouthAmerica[indexPath.row]
+        //return sectionHeaderView
+        default:
+            cell.textLabel?.text = "Other"
+        }
+ */
+        
+        cell.descLabel.text = desc[indexPath.section]
+        cell.titleLabel.text = titles[indexPath.section]
         
         return cell
         
@@ -66,5 +112,4 @@ class ApplicationChecklistViewController: UIViewController, UITableViewDelegate,
         // Pass the selected object to the new view controller.
     }
     */
-
 }
---
+++ ACEios3/ApplicationTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ApplicationTableViewCell.swift
+//  ACEios3
+//
+//  Created by Madelyn Nelson on 2/7/17.
+//  Copyright © 2017 Madelyn Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ApplicationTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
